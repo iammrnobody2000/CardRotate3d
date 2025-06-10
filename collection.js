e6f22497ca33180044f2c25e102adb1ad4ea370a
iammrnobody2000 <139064084+iammrnobody2000@users.noreply.github.com>
2025-06-10 22:14:55 +0530

    Update collection.js

--- collection.js
+++ collection.js
@@ -1,3 +1,23 @@
+import img1 from './img1.jpg'; // Path relative to collection.js
+import img2 from './img2.jpg';
+import img3 from './img3.jpg';
+import img4 from './img4.jpg';
+import img5 from './img5.webp';
+import img6 from './img6.webp';
+import img7 from './img7.webp';
+import img8 from './img8.webp';
+import img9 from './img9.webp';
+import img10 from './img10.webp';
+import img11 from './img11.webp';
+import img12 from './img12.webp';
+import img13 from './img13.webp';
+import img14 from './img14.webp';
+import img15 from './img15.webp';
+import img16 from './img16.webp';
+import img17 from './img17.png';
+import img18 from './img18.webp';
+import img19 from './img19.jpg';
+import img20 from './img20.jpg';
 
 const collection = [
 
